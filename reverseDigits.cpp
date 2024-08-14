b8838399cc7d5dfa8777a044c3f9ce713d213e0d
aasif057 <mdaasifxii@gmail.com>
2024-08-14 23:31:50 +0530

    New program added

---
+++ reverseDigits.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+void reverseN(int a){
+  while(a>=1){
+    int x;
+    x = a%10;
+    a/=10;
+    cout << x;
+  }
+  cout << endl;
+}
+int main(){
+  cout << "Enter a number to reverse: ";
+  int n;
+  cin >> n;
+  cout << "Reversed Numeber is : ";
+  reverseN(n);
+}
+
