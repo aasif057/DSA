312023d47cd827e272ef2554f0861a594a7d749b
Aasif <anony_arch@arch.localdomain>
2025-07-13 13:08:46 +0530

    2 sum variant 2 better and optimal approach added

--- Arrays/medium2.cpp
+++ Arrays/medium2.cpp
@@ -26,6 +26,51 @@ Result brute_force(int arr[], int n, int k){
     return r;
 }
 
+Result optimal(int arr[], int n, int k){
+    Result r;
+    r.value1=-1;
+    r.value2=-1;
+    map<int, int> mpp;
+    for(int i=0;i<n;i++){
+        mpp[arr[i]]=i;
+    }
+    sort(arr, arr+n);
+    int i=0, j=n-1;
+    while(i<j){
+        int sum=arr[i]+arr[j];
+        // cout << 'd' << sum << endl;
+        if(sum<k){
+            i++;
+        }
+        else if(sum>k){
+            j--;
+        }
+        else{
+            r.value1=mpp[arr[i]];
+            r.value2=mpp[arr[j]];
+            return r;
+        }
+    }
+    return r;
+}
+
+Result better(int arr[], int n, int k){
+    map<int, int> mpp;
+    Result r;
+    r.value1=-1;
+    r.value2=-1;
+    for(int i=0;i<n;i++){
+        int a=arr[i];
+        int diff=k-a;
+        if(mpp.find(diff)!=mpp.end()){
+            r.value1=i;
+            r.value2=mpp[diff];
+            return r;
+        }
+        mpp[a]=i;
+    }
+    return r;
+}
 
 int main(){
     int n;
@@ -36,6 +81,6 @@ int main(){
     }
     int k;
     cin >> k;
-    Result r = brute_force(arr,n,k);
+    Result r = optimal(arr,n,k);
     cout << '[' <<r.value1 << ",  " << r.value2 << ']' << endl; 
 }
